7cc206796e19c688e3d7c3078aebd80dc49d4192
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2021-03-15 08:01:26 -0400

    fixes and additional datatype support

--- testing/src/main.c
+++ testing/src/main.c
@@ -1,29 +1,39 @@
 #include <logging.h>
 #include <stdlib.h>
 
-#define formatter_float_0 "%f"
-#define formatter_float_1 "&%f"
+
+#define formatter_float_0 "%g"
+#define formatter_float_1 "&%g"
+
+#define formatter_double_0 "%g"
+#define formatter_double_1 "&%g"
 
 #define formatter_int_0 "%i"
 #define formatter_int_1 "&%i"
 
+#define formatter_long_0 "%li"
+#define formatter_long_1 "&%li"
+
+#define formatter_u_int_0 "%u"
+#define formatter_u_int_1 "&%u"
+
+#define formatter_u_long_0 "%lu"
+#define formatter_u_long_1 "&%lu"
+
 #define formatter_char_0 "%c"
-#define formatter_char_1 "%s"
+#define formatter_char_1 "&%s"
 
 #define formatter(type,indirection) formatter_##type##_##indirection
 
 // char * must be derefrenced 1 less than other arrays
-#define derefOffset(type) (type==char)
+
 
 #define star_0
 #define star_1 * star_0
 #define star_2 * star_1
 #define star(n) star_##n
 
-
-
-
-#define DECLARE_PRINT_ARR_FUNC(type,indirection)                                \
+#define DECLARE_PRINT_ARR_FUNC(type,indirection,derefAmount)                    \
     char *arrStr_##type##_##indirection(type * star(indirection) arr, int len)  \
 {                                                                               \
     int buffReallocSize = len;                                                  \
@@ -37,10 +47,9 @@
     int headRoom;                                                               \
     for(i = 0; i < len; i++){                                                   \
         logDebug("Looping");                                                    \
-        logDebug("%s\n",arr[0]);                                                \
+                                                                                \
         while(1){                                                               \
-            sizeNeeded = snprintf(buffer+writeIndex, buffSize-writeIndex, formatter(type,indirection), arr[i]); \
-            logDebug("%f",arr[i]);                                              \
+            sizeNeeded = snprintf(buffer+writeIndex, buffSize-writeIndex, formatter(type,indirection), star(derefAmount) arr[i]); \
             logDebug(formatter(type,indirection),arr[i]);                       \
             logDebug(buffer);                                                   \
             headRoom = buffSize - writeIndex - sizeNeeded + 1;                  \
@@ -66,25 +75,43 @@
     return buffer;                                                              \
 }
 
-DECLARE_PRINT_ARR_FUNC(int,0);
-DECLARE_PRINT_ARR_FUNC(float,0);
-DECLARE_PRINT_ARR_FUNC(float,1);
+DECLARE_PRINT_ARR_FUNC(int,0,0);
+DECLARE_PRINT_ARR_FUNC(int,1,1);
 
-DECLARE_PRINT_ARR_FUNC(char,0);
+DECLARE_PRINT_ARR_FUNC(long,0,0);
+DECLARE_PRINT_ARR_FUNC(long,1,1);
 
-DECLARE_PRINT_ARR_FUNC(char,1);
+DECLARE_PRINT_ARR_FUNC(u_int,0,0);
+DECLARE_PRINT_ARR_FUNC(u_int,1,1);
 
-#define arrStr(arr, len) _Generic((arr), \
-            float*: arrStr_float_0, \
-            float**: arrStr_float_1, \
-            int*: arrStr_int_0,  \
-            char*: arrStr_char_0, \
-            char**: arrStr_char_1 \
+DECLARE_PRINT_ARR_FUNC(u_long,0,0);
+DECLARE_PRINT_ARR_FUNC(u_long,1,1);
+
+DECLARE_PRINT_ARR_FUNC(float,0,0);
+DECLARE_PRINT_ARR_FUNC(float,1,1);
+
+DECLARE_PRINT_ARR_FUNC(double,0,0);
+DECLARE_PRINT_ARR_FUNC(double,1,1);
+
+DECLARE_PRINT_ARR_FUNC(char,0,0);
+DECLARE_PRINT_ARR_FUNC(char,1,0);
+
+#define arrStr(arr, len) _Generic((arr),                            \
+    float*:  arrStr_float_0, double*:           arrStr_double_0,    \
+    float**: arrStr_float_1, double**:          arrStr_double_1,    \
+    int*:    arrStr_int_0,   unsigned int*:     arrStr_u_int_0,     \
+    int**:   arrStr_int_1,   unsigned int**:    arrStr_u_int_1,     \
+    long*:   arrStr_long_0,  unsigned long*:    arrStr_u_long_0,    \
+    long**:  arrStr_long_1,  unsigned long**:   arrStr_u_long_0,    \
+    char*:   arrStr_char_0,                                         \
+    char**:  arrStr_char_1,                                         \
+    default: arrStr_long_0                                          \
 )(arr, len)
+
 
 int main(){
     //setLogOutput(stdout); Not needed, output defaults to stdout
-    setLogLevel(debug);
+    setLogLevel(info);
     logDebug("message from %s","debug");
     logInfo("message from %s","info");
     logWarn("message from %s","warning");
@@ -93,7 +120,7 @@ int main(){
 
 
     setLogFile("testing/build/testing.log");
-    setLogLevel(debug);
+    setLogLevel(info);
     logDebug("message from %s","debug");
     logInfo("message from %s","info");
     logWarn("message from %s","warning");
@@ -104,7 +131,7 @@ int main(){
     setLogOutput(stdout);
 
     int q = 4;
-    float arr[4] =  {100000000.0, 1.0, 2.0, 3.0};
+    float arr[4] =  {0.1, 1.1, 2.1, 3.1};
     float **ref = malloc(4*sizeof(float*));
     ref[0] = &arr[0];
     ref[1] = &arr[1];
@@ -112,6 +139,8 @@ int main(){
     ref[3] = &arr[3];
 
     #include <string.h>
+
+    int intArr[4] = {0,1,2,3};
 
     char *hello = malloc(6*sizeof(char));
     strcpy(hello,"hello");
@@ -125,11 +154,19 @@ int main(){
     char *you =  malloc(4*sizeof(char));
     strcpy(you,"you");
 
-    char **strArr = {&hello,&how,&are,&you};
-    
+    char *strArr[4];
+
+    strArr[0] = hello;
+    strArr[1] = how;
+    strArr[2] = are;
+    strArr[3] = you;
+
+
     printf("%f\n",*ref[3]);
-    return;
+
     printf("%s\n",arrStr(arr,q));
-    //printf("%s\n",arrStr(strArr,3));
+    printf("%s\n",arrStr(strArr,4));
+    printf("%s\n",arrStr(hello,5));
+    printf("%s\n",arrStr(intArr,4));
     logFatal("message from %s","fatal");
 }
